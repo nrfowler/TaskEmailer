6eca1621932de8613dc4b91c48df747589ffc1d6
nrfowler <nrfowler@gmail.com>
2019-04-15 11:01:04 -0500

    A simple .NET console app that emails a list of tasks in a file to an email address.

--- TaskEmailer/Program.cs
+++ TaskEmailer/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net.Mail;
+using System.Text;
 
 namespace TaskEmailer
 {
@@ -6,7 +9,28 @@ namespace TaskEmailer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var lines = File.ReadLines(args[2]);
+            foreach(var line in lines)
+                SendMail(args[0],args[1],line, line);
+        }
+
+        private static void SendMail(string username, string password, string sub, string body)
+        {
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential(username,password);
+
+            MailMessage mm = new MailMessage("nrfowler@gmail.com", "nrfowler@gmail.com", sub, body);
+            mm.BodyEncoding = UTF8Encoding.UTF8;
+            mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+            client.Send(mm);
         }
     }
 }
